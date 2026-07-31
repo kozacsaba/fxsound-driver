5fa8072bc8acfb648d59f4d954448bca5c921a43
Koza Csaba <kozacsaba@gmail.com>
2026-07-31 16:32:08 +0200

    refactor from hardcoded wdk files to assets

--- cmake/WindowsDriver.cmake
+++ cmake/WindowsDriver.cmake
@@ -1,63 +1,29 @@
 include_guard(GLOBAL)
 
-function(_wdk_write_driver_props output_file)
-    file(WRITE "${output_file}" [=[<?xml version="1.0" encoding="utf-8"?>
-<Project ToolsVersion="Current" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-  <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props"
-          Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" />
-  <ItemDefinitionGroup>
-    <ClCompile>
-      <AdditionalIncludeDirectories>$(DDK_INC_PATH);%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-    </ClCompile>
-    <ResourceCompile>
-      <AdditionalIncludeDirectories>$(DDK_INC_PATH);%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-    </ResourceCompile>
-    <DriverSign>
-      <AdditionalOptions>/fd SHA256 %(AdditionalOptions)</AdditionalOptions>
-    </DriverSign>
-  </ItemDefinitionGroup>
-</Project>
-]=])
-endfunction()
+function(_wdk_copy_asset asset_name output_file)
+    set(asset_file "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/wdk/${asset_name}")
 
-function(_wdk_write_package_props output_file)
-    file(WRITE "${output_file}" [=[<?xml version="1.0" encoding="utf-8"?>
-<Project ToolsVersion="Current" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-  <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props"
-          Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" />
-  <ItemDefinitionGroup>
-    <DriverSign>
-      <AdditionalOptions>/fd SHA256 %(AdditionalOptions)</AdditionalOptions>
-    </DriverSign>
-  </ItemDefinitionGroup>
-</Project>
-]=])
-endfunction()
+    if(NOT EXISTS "${asset_file}")
+        message(FATAL_ERROR "Missing WDK build asset: ${asset_file}")
+    endif()
 
-function(_wdk_write_driver_targets output_file)
-    file(WRITE "${output_file}" [=[<?xml version="1.0" encoding="utf-8"?>
-<Project ToolsVersion="Current"
-         xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-  <ItemGroup>
-    <FilesToPackage
-        Include="$(TargetPath)"
-        Condition="'$(ConfigurationType)' == 'Driver'" />
-  </ItemGroup>
-</Project>
-]=])
+    configure_file(
+        "${asset_file}"
+        "${output_file}"
+        COPYONLY
+    )
 endfunction()
 
 function(configure_wdm_driver target)
     if(NOT TARGET "${target}")
-        message(FATAL_ERROR
-            "configure_wdm_driver: unknown target '${target}'")
+        message(FATAL_ERROR "configure_wdm_driver: unknown target '${target}'")
     endif()
 
     set(props_file "${CMAKE_CURRENT_BINARY_DIR}/${target}.wdk.props")
     set(targets_file "${CMAKE_CURRENT_BINARY_DIR}/${target}.wdk.targets")
 
-    _wdk_write_driver_props("${props_file}")
-    _wdk_write_driver_targets("${targets_file}")
+    _wdk_copy_asset(driver.props "${props_file}")
+    _wdk_copy_asset(driver.targets "${targets_file}")
 
     set_target_properties("${target}" PROPERTIES
         VS_CONFIGURATION_TYPE "Driver"
@@ -79,9 +45,7 @@ function(configure_wdm_driver target)
     )
 
     # CMake otherwise enables normal user-mode manifest generation.
-    target_link_options("${target}" PRIVATE
-        "/MANIFEST:NO"
-    )
+    target_link_options("${target}" PRIVATE "/MANIFEST:NO")
 
     # Reproduce the libraries used by the original WDK project.
     #
@@ -95,7 +59,8 @@ function(configure_wdm_driver target)
         portcls.lib
         stdunk.lib
 
-        # CMake imports full paths ending in .targets into the VS project.
+        # Imported into the generated Visual Studio project as an
+        # MSBuild .targets file.
         "${targets_file}"
     )
 endfunction()
@@ -103,14 +68,17 @@ endfunction()
 function(add_wdk_driver_package package_target driver_target)
     if(NOT TARGET "${driver_target}")
         message(FATAL_ERROR
-            "add_wdk_driver_package: unknown driver target '${driver_target}'")
+            "add_wdk_driver_package: unknown driver target "
+            "'${driver_target}'"
+        )
     endif()
 
     add_custom_target("${package_target}" ALL)
     add_dependencies("${package_target}" "${driver_target}")
 
     set(props_file "${CMAKE_CURRENT_BINARY_DIR}/${package_target}.wdk.props")
-    _wdk_write_package_props("${props_file}")
+
+    _wdk_copy_asset(package.props "${props_file}")
 
     set_target_properties("${package_target}" PROPERTIES
         VS_CONFIGURATION_TYPE "Utility"
