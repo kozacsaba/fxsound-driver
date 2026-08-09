4b8ffb60f07396e21c11662bcb1eaf1dccc7863a
Koza Csaba <kozacsaba@gmail.com>
2026-08-09 13:58:51 +0200

    identity templates

--- cmake/DriverIdentity.cmake
+++ cmake/DriverIdentity.cmake
@@ -15,6 +15,16 @@ set(FXVAD_ICON ""
     "Optional driver icon; empty uses the existing FxSound icon"
 )
 
+set(FXVAD_SPEAKER_GUID "{6518B5E9-772E-48E2-B110-1752F65934E8}"
+    CACHE STRING
+    "Stable product-specific speaker media category GUID"
+)
+
+set(FXVAD_WEBSITE ""
+    CACHE STRING
+    "Optional vendor website"
+)
+
 function(configure_driver_identity target)
     if(NOT TARGET "${target}")
         message(FATAL_ERROR
@@ -44,23 +54,42 @@ function(configure_driver_identity target)
     message(STATUS "Driver identity configuration:")
     message(STATUS "  FXVAD_ID = \"${FXVAD_ID}\"")
     message(STATUS "  FXVAD_DISPLAY_NAME = \"${FXVAD_DISPLAY_NAME}\"")
-    message(STATUS "  FXVAD_ICON = \"${FXVAD_ICON}\"")
+    message(STATUS "  FXVAD_ICON = \"${DRIVER_ICON}\"")
+    message(STATUS "  FXVAD_SPEAKER_GUID = \"${FXVAD_SPEAKER_GUID}\"")
+    message(STATUS "  FXVAD_WEBSITE = \"${FXVAD_WEBSITE}\"")
+
 
     string(TOUPPER "${FXVAD_ID}" DRIVER_ID_UPPER)
 
-    set(DRIVER_ID           "${FXVAD_ID}")
+    set(DRIVER_ID "${FXVAD_ID}")
+    set(DRIVER_DEVICE_ID "${DRIVER_ID_UPPER}")
+    set(DRIVER_SERVICE_NAME "${DRIVER_ID_UPPER}")
+
+    set(DRIVER_SYS_NAME "${FXVAD_ID}.sys")
+    set(DRIVER_INF_NAME "${FXVAD_ID}.inf")
+
+    set(DRIVER_CAT_X86_NAME "${FXVAD_ID}NTx86.cat")
+    set(DRIVER_CAT_AMD64_NAME "${FXVAD_ID}NTAMD64.cat")
+
     set(DRIVER_DISPLAY_NAME "${FXVAD_DISPLAY_NAME}")
-
-    set(DRIVER_SYS_NAME     "${FXVAD_ID}.sys")
-    set(DRIVER_INF_NAME     "${FXVAD_ID}.inf")
-    set(DRIVER_CAT_NAME     "${FXVAD_ID}ntamd64.cat")
-
-    set(DRIVER_SERVICE_NAME "${FXVAD_ID}")
-    set(DRIVER_HARDWARE_ID  "ROOT\\${DRIVER_ID_UPPER}")
-
+    set(DRIVER_DEVICE_NAME "${FXVAD_DISPLAY_NAME} Audio Enhancer")
     set(DRIVER_PROVIDER_NAME "${FXVAD_DISPLAY_NAME}")
-    set(DRIVER_DEVICE_NAME   "${FXVAD_DISPLAY_NAME} Virtual Audio Device")
-    set(DRIVER_ENDPOINT_NAME "${FXVAD_DISPLAY_NAME} Speakers")
+
+    set(DRIVER_WAVE_NAME "${FXVAD_DISPLAY_NAME} Wave")
+    set(DRIVER_TOPOLOGY_NAME "${FXVAD_DISPLAY_NAME} Topology")
+    set(DRIVER_SPEAKER_NAME "${FXVAD_DISPLAY_NAME} Speakers")
+
+    set(DRIVER_SPEAKER_GUID "${FXVAD_SPEAKER_GUID}")
+
+    if(FXVAD_WEBSITE)
+        set(DRIVER_VENDOR_WEBSITE_LINE
+            "DeviceVendorWebSite,,,,\"${FXVAD_WEBSITE}\""
+        )
+    else()
+        set(DRIVER_VENDOR_WEBSITE_LINE
+            ";DeviceVendorWebSite not configured"
+        )
+    endif()
 
     file(MAKE_DIRECTORY "${binary_dir}/generated")
 
@@ -84,6 +113,7 @@ function(configure_driver_identity target)
 
     set_source_files_properties(
         "${generated_inf}"
+        TARGET_DIRECTORY "${target}"
         PROPERTIES
             VS_TOOL_OVERRIDE "Inf"
             VS_SETTINGS "TimeStamp=14.0.0.1"
